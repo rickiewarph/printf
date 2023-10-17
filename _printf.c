d1b7c83c2c1282957986c440e318ea0e0ff2d385
lndukuo <leonardndukuo@gmail.com>
2023-10-17 06:26:59 -0700

    _printf.c edit

--- _printf.c
+++ _printf.c
@@ -9,7 +9,11 @@
 int _printf(const char *format, ...)
 {
 	match m[] = {
-		{"%c", printf_char}, {"%s", printf_string}, {"%%", print_37}, {"%d", printf_dec}, {"%i", printf_int}, {"%r", print_rev}, {"%R", print_rot13}, {"%b", print_binary}, {"%u", printf_unsigned_int}, {"%o", print_oct}, {"%x", print_hex}, {"%X", PRINT_HEX}, {"%S", print_STRING}, {"%p", print_pointer}
+		{"%c", printf_char}, {"%s", printf_string},
+		{"%%", print_37}, {"%d", printf_dec}, {"%i", printf_int},
+		{"%r", print_rev}, {"%R", print_rot13}, {"%b", print_binary},
+		{"%u", printf_unsigned_int}, {"%o", print_oct}, {"%x", print_hex},
+		{"%X", PRINT_HEX}, {"%S", print_STRING}, {"%p", print_pointer}
 	};
 
 	va_list args;
